1749dc8d2f840d20b2292a28ceea115fe093f37f
tiwari-anushka <anushk.tiwari.2005@gmail.com>
2026-06-04 14:44:12 +0530

    Create RemoveDuplicatesFromSortedArray.py

---
+++ day05/RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        if len(nums)<=2:
+            return len(nums)
+        write = 2 
+
+        for read in range(2, len(nums)):
+            if nums[read] != nums[write - 2]:
+                nums[write] = nums[read]
+                write += 1
+                
+        return write
